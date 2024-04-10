1a6e22cf08b1f9b4e1eaeb685b69f452644c1c76
Daniil Zaonegin <zaoneg@gmail.com>
2024-04-10 22:09:54 +0200

    fix

--- src/BudgetManager/Program.cs
+++ src/BudgetManager/Program.cs
@@ -5,6 +5,7 @@ using BudgetManager.Data;
 using BudgetManager.Services;
 using BudgetManager.Shared;
 using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.FluentUI.AspNetCore.Components;
@@ -76,7 +77,10 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-app.UseForwardedHeaders();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedProto
+});
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
